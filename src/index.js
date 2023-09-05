ef93f0b1a558991cadf1351cff76c580a5ade214
Jenna0430 <114163480+Jenna0430@users.noreply.github.com>
2023-09-05 10:51:01 +0100

    importing the api link used to get and display forecast

--- src/index.js
+++ src/index.js
@@ -69,7 +69,13 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+//importing api link for displaying forecast
+function getForecast(coordinates) {
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
+  axios.get(apiUrl).then(displayForecast);
+}
 
 
 
